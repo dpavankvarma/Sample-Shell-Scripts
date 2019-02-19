dc37946b9ac02c97ffd44ce5fd214b8faf62c320
Pavan Kumar Varma <dpavankvarma@gmail.com>
2019-02-19 12:03:47 +0530

    Added New Script

---
+++ 6_sum_diff_mult_remainder.sh
@@ -0,0 +1,11 @@
+# Problem : Given two integers,  and , find their sum, difference, product, and quotient.
+
+#!/bin/bash
+
+read X 
+read Y
+
+echo $((X+Y))
+echo $((X-Y))
+echo $((X*Y))
+echo $((X/Y))
